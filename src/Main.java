3f1bb029f489d6a588605710b91cd6a619152033
ANDRE <klai1@mail.ru>
2022-08-01 22:10:44 +0400

    Задача 3 выполнено

--- src/Main.java
+++ src/Main.java
@@ -13,9 +13,25 @@ public class Main {
         }
         System.out.println();
         //Задача 2
-        int friday=1;
+        int friday=5;
         for (;friday<=31;friday=friday+7)
         {System.out.println("Сегодня пятница, "+ friday+ "-е число. Необходимо подготовить отчет ");}
+
+        //Задание 3
+        int yearNew2=2020-200;
+        int yearNew=2020+100;
+
+        for (int year=0;year<=2020+100;year=year+79){
+            if(year>=yearNew2&&year<=yearNew){
+
+            System.out.print(year+" ");}
+                }
+
+
+
+
+
+
 
     }
 }
